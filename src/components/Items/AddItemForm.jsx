97690395f5c2fb30a79d24c6089f147888ba00dc
mominahmed23 <mominahmad324@gmail.com>
2021-08-05 13:12:30 +0500

    in progress

--- src/components/Items/AddItemForm.jsx
+++ src/components/Items/AddItemForm.jsx
@@ -3,20 +3,22 @@ import { Box, TextField, Button, MenuItem, Grid } from "@material-ui/core";
 import { useForm } from "react-hook-form";
 import { generateId } from "../../utils/common";
 import { useState } from "react";
-import Select from '@material-ui/core/Select';
-import InputLabel from '@material-ui/core/InputLabel';
+import Select from "@material-ui/core/Select";
+import InputLabel from "@material-ui/core/InputLabel";
 import FormModal from "../common/FormModal";
 import AddCategoryForm from "../Categories/AddCategoryForm";
 
 const AddItemForm = ({ submitProjectData, categories, submitCategoryData }) => {
-  const { register, formState: { errors }, handleSubmit } = useForm();
-  const [newCategoryModel, setNewCategoryModel] = useState(false)
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  const [newCategoryModel, setNewCategoryModel] = useState(false);
+
+  console.log(errors);
 
   const [open, setOpen] = React.useState(false);
-
-  // const handleChange = (event) => {
-  //   setAge(event.target.value);
-  // };
 
   const handleCloseDropDown = () => {
     setOpen(false);
@@ -35,178 +37,202 @@ const AddItemForm = ({ submitProjectData, categories, submitCategoryData }) => {
   const onNewCategoryClick = () => {
     setNewCategoryModel(true);
   };
-  return(
-      <div>
-        {newCategoryModel && (
-          <FormModal
-            title="Add New Category"
-            open={newCategoryModel}
+  return (
+    <div>
+      {newCategoryModel && (
+        <FormModal
+          title="Add New Category"
+          open={newCategoryModel}
+          handleClose={handleClosecategoryModal}
+        >
+          <AddCategoryForm
+            submitCategoryData={submitCategoryData}
             handleClose={handleClosecategoryModal}
-          >
-            <AddCategoryForm submitCategoryData={submitCategoryData} handleClose={handleClosecategoryModal}/>
-          </FormModal>
-        )}
-        
-     <form onSubmit={handleSubmit(submitData)}>
-       <Grid container spacing={3}>
-         <Grid item xs={12} sm={6}>
-          <TextField
-              {...register("product", { required: true })}
-                fullWidth
-                label="Product"
-                margin="dense"
-                variant="outlined"
-              />
-            <span style={{color:"red"}}> {errors.product && 'required'}</span>
-         </Grid>
-         <Grid item xs={12}  sm={6}> 
+          />
+        </FormModal>
+      )}
+
+      <form onSubmit={handleSubmit(submitData)}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6}>
             <TextField
-            {...register("quantity", { required: true })}
-            fullWidth
-            label="Quantity"
-            margin="dense"
-            variant="outlined"
-          />
-          <span style={{color:"red"}}> {errors.quantity && 'required'}</span>
-         </Grid>
-       <Grid item xs={12}  sm={6}> 
-          <div style={{display:"flex", justifyContent:"space-between", alignItems:"center", margin:"15px 0px"}}> 
-          <div style={{flexGrow:1,marginRight:"5px"}}>
-          <InputLabel >Category</InputLabel>
-          <Select
+              {...register("product", { required: true })}
+              fullWidth
+              label="Product"
+              margin="dense"
+              variant="outlined"
+            />
+            <span style={{ color: "red" }}>
+              {" "}
+              {errors.product && "required"}
+            </span>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              {...register("quantity", { required: true })}
+              fullWidth
+              label="Quantity"
+              margin="dense"
+              variant="outlined"
+            />
+            <span style={{ color: "red" }}>
+              {" "}
+              {errors.quantity && "required"}
+            </span>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                margin: "15px 0px",
+              }}
+            >
+              <div style={{ flexGrow: 1, marginRight: "5px" }}>
+                <InputLabel>Category</InputLabel>
+                <Select
+                  {...register("categoryId", { required: true })}
+                  open={open}
+                  onClose={handleCloseDropDown}
+                  onOpen={handleOpenDropDown}
+                  fullWidth
+                >
+                  {categories.length ? (
+                    categories.map((item) => (
+                      <MenuItem value={item.id}>{item.name}</MenuItem>
+                    ))
+                  ) : (
+                    <MenuItem value={null}>None</MenuItem>
+                  )}
+                </Select>
+              </div>
+              <div>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  disableElevation
+                  onClick={onNewCategoryClick}
+                >
+                  Add new category
+                </Button>
+              </div>
+            </div>
+            <span style={{ color: "red" }}>
+              {" "}
+              {errors.categoryId && "required"}
+            </span>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              {...register("itemCode", { required: true })}
+              fullWidth
+              label="Item Code/ Bar Code"
+              margin="dense"
+              variant="outlined"
+            />
+            <span style={{ color: "red" }}>
+              {" "}
+              {errors.itemCode && "required"}
+            </span>
+          </Grid>
 
-          {...register("categoryId",{ required: true })}
-
-            open={open}
-            onClose={handleCloseDropDown}
-            onOpen={handleOpenDropDown}
-          fullWidth
-
-          >
-          {categories.length ? (
-              categories.map((item) => (
-                <MenuItem value={item.id}>{item.name}</MenuItem>
-              ))
-          ) :(
-            <MenuItem value={null}>None</MenuItem>
-          )}
-
-          
-          </Select>
-          </div>
-          <div>
-          <Button
-            variant="contained"
-            color="secondary"
-            disableElevation
-            onClick={onNewCategoryClick}
-          >
-            Add new category
-          </Button>
-          </div>
-          </div>
-          <span style={{color:"red"}}> {errors.categoryId && 'required'}</span>
-      </Grid>
-      <Grid item xs={12} sm={6}>
-        <TextField
-          {...register("itemCode", { required: true })}
-          fullWidth
-          label="Item Code/ Bar Code"
-          margin="dense"
-          variant="outlined"
-        />
-        <span style={{color:"red"}}> {errors.itemCode && 'required'}</span>
-      </Grid>
-     
-      <Grid item xs={12} sm={6}>
-          <TextField
-            {...register("hsn", { required: true })}
-            fullWidth
-            label="HSN/SAC Code"
-            margin="dense"
-            variant="outlined"
-          />
-          <span style={{color:"red"}}> {errors.hsn && 'required'}</span>
-      </Grid>
-      <Grid item xs={12} sm={6}>
-      <TextField
-        {...register("salesPrice", { required: true })}
-        fullWidth
-        label="Sales Price"
-        margin="dense"
-        variant="outlined"
-      />
-      <span style={{color:"red"}}> {errors.salesPrice && 'required'}</span>
-      </Grid>
-      <Grid item xs={12} sm={6}>
-      <TextField
-        {...register("purchasePrice", { required: true })}
-        fullWidth
-        label="Purchase Price"
-        margin="dense"
-        variant="outlined"
-      />
-      <span style={{color:"red"}}> {errors.purchasePrice && 'required'}</span>
-      </Grid>
-      <Grid item xs={12} sm={6}>
-      <TextField
-        {...register("mrp", { required: true })}
-        fullWidth
-        label="MRP"
-        margin="dense"
-        variant="outlined"
-      />
-      <span style={{color:"red"}}> {errors.mrp && 'required'}</span>
-      </Grid>
-      <Grid item xs={12} sm={6}>
-      <TextField
-        {...register("discount", { required: true })}
-        fullWidth
-        label="Discount"
-        margin="dense"
-        variant="outlined"
-      />
-      <span style={{color:"red"}}> {errors.discount && 'required'}</span>
-      </Grid>
-      <Grid item xs={12} sm={6}>
-      <TextField
-        {...register("texSlab", { required: true })}
-        fullWidth
-        label="Tex Slab"
-        margin="dense"
-        variant="outlined"
-      />
-      <span style={{color:"red"}}> {errors.texSlab && 'required'}</span>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              {...register("hsn", { required: true })}
+              fullWidth
+              label="HSN/SAC Code"
+              margin="dense"
+              variant="outlined"
+            />
+            <span style={{ color: "red" }}> {errors.hsn && "required"}</span>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              {...register("salesPrice", { required: true })}
+              fullWidth
+              label="Sales Price"
+              margin="dense"
+              variant="outlined"
+            />
+            <span style={{ color: "red" }}>
+              {" "}
+              {errors.salesPrice && "required"}
+            </span>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              {...register("purchasePrice", { required: true })}
+              fullWidth
+              label="Purchase Price"
+              margin="dense"
+              variant="outlined"
+            />
+            <span style={{ color: "red" }}>
+              {" "}
+              {errors.purchasePrice && "required"}
+            </span>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              {...register("mrp", { required: true })}
+              fullWidth
+              label="MRP"
+              margin="dense"
+              variant="outlined"
+            />
+            <span style={{ color: "red" }}> {errors.mrp && "required"}</span>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              {...register("discount", { required: true })}
+              fullWidth
+              label="Discount"
+              margin="dense"
+              variant="outlined"
+            />
+            <span style={{ color: "red" }}>
+              {" "}
+              {errors.discount && "required"}
+            </span>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              {...register("texSlab", { required: true })}
+              fullWidth
+              label="Tex Slab"
+              margin="dense"
+              variant="outlined"
+            />
+            <span style={{ color: "red" }}>
+              {" "}
+              {errors.texSlab && "required"}
+            </span>
           </Grid>
           <Grid item xs={12} sm={12}>
-      <TextField
-        {...register("comment")}
-        fullWidth
-        multiline
-        label="Comments"
-        margin="dense"
-        variant="outlined"
-      />
-       </Grid>
-      
-      </Grid>
-      <Box marginY={3} textAlign="right">
-        <Button
-          
-          type="submit"
-          variant="contained"
-          color="primary"
-          disableElevation
-        >
-          save
-        </Button>
-      </Box>
+            <TextField
+              {...register("comment")}
+              fullWidth
+              multiline
+              label="Comments"
+              margin="dense"
+              variant="outlined"
+            />
+          </Grid>
+        </Grid>
+        <Box marginY={3} textAlign="right">
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            disableElevation
+          >
+            save
+          </Button>
+        </Box>
       </form>
-      </div>
-
-    
-    );
+    </div>
+  );
 };
 
 export default AddItemForm;
-
--- src/components/Items/EditItemForm.jsx
+++ src/components/Items/EditItemForm.jsx
@@ -1,29 +1,44 @@
 import React from "react";
-import { Box, TextField, Button, MenuItem,Grid } from "@material-ui/core";
+import { Box, TextField, Button, MenuItem, Grid } from "@material-ui/core";
 import { useForm } from "react-hook-form";
-import { generateId } from "../../utils/common";
 import { useState } from "react";
-import Select from '@material-ui/core/Select';
-import InputLabel from '@material-ui/core/InputLabel';
+import Select from "@material-ui/core/Select";
+import InputLabel from "@material-ui/core/InputLabel";
 import FormModal from "../common/FormModal";
 import AddCategoryForm from "../Categories/AddCategoryForm";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { editItemAction } from "../../redux/actions/items";
-const EditItemForm = ({ submitProjectData,submitCategoryData,categories, editId,handleEditClose }) => {
+const EditItemForm = ({
+  submitProjectData,
+  submitCategoryData,
+  categories,
+  editId,
+  handleEditClose,
+}) => {
   const dispatch = useDispatch();
-  const {items} = useSelector(state => state);
+  const { items } = useSelector((state) => state);
+  const itemData = items.find((x) => x.id === editId);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
+    defaultValues: {
+      product: itemData.product,
+      quantity: itemData.quantity,
+    },
+  });
 
-const itemData = items.find(x => x.id === editId);
+  console.log(itemData);
 
-const submitData = (data) => {
-  dispatch(editItemAction(data, editId));
-  handleEditClose();
-};
-  
-  
-const { register, formState: { errors }, handleSubmit } = useForm();
-  const [newCategoryModel, setNewCategoryModel] = useState(false)
+  const submitData = (data) => {
+    dispatch(editItemAction(data, editId));
+    handleEditClose();
+  };
+
+  console.log(errors);
+  const [newCategoryModel, setNewCategoryModel] = useState(false);
 
   const [open, setOpen] = React.useState(false);
   const handleCloseDropDown = () => {
@@ -41,183 +56,206 @@ const { register, formState: { errors }, handleSubmit } = useForm();
   };
   return (
     <div>
-    {newCategoryModel && (
-      <FormModal
-        title="Add New Category"
-        open={newCategoryModel}
-        handleClose={handleClosecategoryModal}
-      >
-        <AddCategoryForm submitCategoryData={submitCategoryData} handleClose={handleClosecategoryModal}/>
-      </FormModal>
-    )}
-    {!!itemData &&
-      <form onSubmit={handleSubmit(submitData)}>
-        <Grid container spacing={3}>
-         <Grid item xs={12} sm={6}>
-          <TextField
-            {...register("product", { required: true })}
-            fullWidth
-            label="Product"
-            margin="dense"
-            variant="outlined"
-            defaultValue={itemData.product}
-          />
-          <span style={{color:"red"}}> {errors.product && 'required'}</span>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-          {...register("quantity", { required: true })}
-            fullWidth
-            label="Quantity"
-            margin="dense"
-            variant="outlined"
-            defaultValue={itemData.quantity}
-          />
-          <span style={{color:"red"}}> {errors.quantity && 'required'}</span>
-        </Grid>
-    <Grid item xs={12} sm={6}>
-    <div style={{display:"flex", justifyContent:"space-between", alignItems:"center", margin:"15px 0px"}}> 
-      <div style={{flexGrow:1,marginRight:"5px"}}>
-      <InputLabel >Category</InputLabel>
-      <Select
-     {...register("categoryId",{ required: true })}
-        open={open}
-        onClose={handleCloseDropDown}
-        onOpen={handleOpenDropDown}
-        fullWidth
-        defaultValue={itemData.categoryId}
-      >
-       {categories.length ? (
-           categories.map((item) => (
-            <MenuItem value={item.id}>{item.name}</MenuItem>
-          ))
-       ) :(
-        <MenuItem value={null}>None</MenuItem>
-       )}
-      
-       
-      </Select>
-      </div>
-      <div>
-      <Button
-          variant="contained"
-          color="secondary"
-          disableElevation
-          onClick={onNewCategoryClick}
+      {newCategoryModel && (
+        <FormModal
+          title="Add New Category"
+          open={newCategoryModel}
+          handleClose={handleClosecategoryModal}
         >
-          Add new category
-        </Button>
-      </div>
-    </div>
-    <span style={{color:"red"}}> {errors.categoryId && 'required'}</span>
-    </Grid>
-    <Grid item xs={12} sm={6}>
-    <TextField
-     {...register("itemCode", { required: true })}
-      fullWidth
-      label="Item Code/ Bar Code"
-      margin="dense"
-      variant="outlined"
-      defaultValue={itemData.itemCode}
-    />
-    <span style={{color:"red"}}> {errors.itemCode && 'required'}</span>
-    </Grid>
-    <Grid item xs={12} sm={6}>
-    <TextField
-      {...register("hsn", { required: true })}
-      fullWidth
-      label="HSN/SAC Code"
-      margin="dense"
-      variant="outlined"
-      defaultValue={itemData.hsn}
-    />
-     <span style={{color:"red"}}> {errors.hsn && 'required'}</span>
-     </Grid>
-     <Grid item xs={12} sm={6}>
-    <TextField
-     {...register("salesPrice", { required: true })}
-      fullWidth
-      label="Sales Price"
-      margin="dense"
-      variant="outlined"
-      defaultValue={itemData.salesPrice}
-    />
-    <span style={{color:"red"}}> {errors.salesPrice && 'required'}</span>
-    </Grid>
-    <Grid item xs={12} sm={6}>
-    <TextField
-     {...register("purchasePrice", { required: true })}
-      fullWidth
-      label="Purchase Price"
-      margin="dense"
-      variant="outlined"
-      defaultValue={itemData.purchasePrice}
-    />
-    <span style={{color:"red"}}> {errors.purchasePrice && 'required'}</span>
-    </Grid>
-    <Grid item xs={12} sm={6}>
-    <TextField
-      {...register("mrp", { required: true })}
-      fullWidth
-      label="MRP"
-      margin="dense"
-      variant="outlined"
-      defaultValue={itemData.mrp}
-    />
-    <span style={{color:"red"}}> {errors.mrp && 'required'}</span>
-    </Grid>
-    <Grid item xs={12} sm={6}>
-    <TextField
-     {...register("discount", { required: true })}
-      fullWidth
-      label="Discount"
-      margin="dense"
-      variant="outlined"
-      defaultValue={itemData.discount}
-    />
-    <span style={{color:"red"}}> {errors.discount && 'required'}</span>
-    </Grid>
-    <Grid item xs={12} sm={6}>
-    <TextField
-      {...register("texSlab", { required: true })}
-      fullWidth
-      label="Tex Slab"
-      margin="dense"
-      variant="outlined"
-      defaultValue={itemData.texSlab}
-    />
-    <span style={{color:"red"}}> {errors.texSlab && 'required'}</span>
-    </Grid>
-    <Grid item xs={12} sm={12}>
-     <TextField
-        {...register("comment")}
-        fullWidth
-        multiline
-        label="Comments"
-        margin="dense"
-        variant="outlined"
-      />
-      </Grid>
-    </Grid>
-    <Box marginY={3} textAlign="right">
-        <Button
-          
-          type="submit"
-          variant="contained"
-          color="primary"
-          disableElevation
-        >
-          save
-        </Button>
-      </Box>
-    </form>
-}
+          <AddCategoryForm
+            submitCategoryData={submitCategoryData}
+            handleClose={handleClosecategoryModal}
+          />
+        </FormModal>
+      )}
+      {!!itemData && (
+        <form onSubmit={handleSubmit(submitData)}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                {...register("product", { required: true })}
+                fullWidth
+                label="Product"
+                margin="dense"
+                variant="outlined"
+                value={itemData.product}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                {...register("quantity", { required: true })}
+                fullWidth
+                label="Quantity"
+                margin="dense"
+                variant="outlined"
+                defaultValue={itemData.quantity}
+              />
+              <span style={{ color: "red" }}>
+                {" "}
+                {errors.quantity && "required"}
+              </span>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                  margin: "15px 0px",
+                }}
+              >
+                <div style={{ flexGrow: 1, marginRight: "5px" }}>
+                  <InputLabel>Category</InputLabel>
+                  <Select
+                    {...register("categoryId", { required: true })}
+                    open={open}
+                    onClose={handleCloseDropDown}
+                    onOpen={handleOpenDropDown}
+                    fullWidth
+                    defaultValue={itemData.categoryId}
+                  >
+                    {categories.length ? (
+                      categories.map((item) => (
+                        <MenuItem value={item.id}>{item.name}</MenuItem>
+                      ))
+                    ) : (
+                      <MenuItem value={null}>None</MenuItem>
+                    )}
+                  </Select>
+                </div>
+                <div>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    disableElevation
+                    onClick={onNewCategoryClick}
+                  >
+                    Add new category
+                  </Button>
+                </div>
+              </div>
+              <span style={{ color: "red" }}>
+                {" "}
+                {errors.categoryId && "required"}
+              </span>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                {...register("itemCode", { required: true })}
+                fullWidth
+                label="Item Code/ Bar Code"
+                margin="dense"
+                variant="outlined"
+                defaultValue={itemData.itemCode}
+              />
+              <span style={{ color: "red" }}>
+                {" "}
+                {errors.itemCode && "required"}
+              </span>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                {...register("hsn", { required: true })}
+                fullWidth
+                label="HSN/SAC Code"
+                margin="dense"
+                variant="outlined"
+                defaultValue={itemData.hsn}
+              />
+              <span style={{ color: "red" }}> {errors.hsn && "required"}</span>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                {...register("salesPrice", { required: true })}
+                fullWidth
+                label="Sales Price"
+                margin="dense"
+                variant="outlined"
+                defaultValue={itemData.salesPrice}
+              />
+              <span style={{ color: "red" }}>
+                {" "}
+                {errors.salesPrice && "required"}
+              </span>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                {...register("purchasePrice", { required: true })}
+                fullWidth
+                label="Purchase Price"
+                margin="dense"
+                variant="outlined"
+                defaultValue={itemData.purchasePrice}
+              />
+              <span style={{ color: "red" }}>
+                {" "}
+                {errors.purchasePrice && "required"}
+              </span>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                {...register("mrp", { required: true })}
+                fullWidth
+                label="MRP"
+                margin="dense"
+                variant="outlined"
+                defaultValue={itemData.mrp}
+              />
+              <span style={{ color: "red" }}> {errors.mrp && "required"}</span>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                {...register("discount", { required: true })}
+                fullWidth
+                label="Discount"
+                margin="dense"
+                variant="outlined"
+                defaultValue={itemData.discount}
+              />
+              <span style={{ color: "red" }}>
+                {" "}
+                {errors.discount && "required"}
+              </span>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                {...register("texSlab", { required: true })}
+                fullWidth
+                label="Tex Slab"
+                margin="dense"
+                variant="outlined"
+                defaultValue={itemData.texSlab}
+              />
+              <span style={{ color: "red" }}>
+                {" "}
+                {errors.texSlab && "required"}
+              </span>
+            </Grid>
+            <Grid item xs={12} sm={12}>
+              <TextField
+                {...register("comment")}
+                fullWidth
+                multiline
+                label="Comments"
+                margin="dense"
+                variant="outlined"
+              />
+            </Grid>
+          </Grid>
+          <Box marginY={3} textAlign="right">
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              disableElevation
+            >
+              save
+            </Button>
+          </Box>
+        </form>
+      )}
     </div>
   );
 };
 
 export default EditItemForm;
-
-
-
-
